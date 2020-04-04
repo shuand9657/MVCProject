8dcdabc9dcdd801374fd405af9b72821222926b6
Andy <andy.fung@tradevan.com.tw>
2020-04-04 14:16:11 +0800

    加入專案檔案。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TestLayout()
+        {
+            ViewBag.ShowMessage = @"
+順便了解ViewBag, ViewData, TempData的差別。
+
+";
+            TempData["Temp"] = "I will Gone";
+
+            return View();
+        }
+    }
+}
